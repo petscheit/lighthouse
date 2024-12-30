434782e5c3b0dd09bed26b0d51998781b6ba5a81
petscheit <etscheitp@gmail.com>
2024-12-30 15:15:00 +0100

    feat: add TreeHash derive to beacon body

--- consensus/types/src/beacon_block_body.rs
+++ consensus/types/src/beacon_block_body.rs
@@ -62,11 +62,12 @@ pub const BLOB_KZG_COMMITMENTS_INDEX: usize = 11;
     cast_error(ty = "Error", expr = "Error::IncorrectStateVariant"),
     partial_getter_error(ty = "Error", expr = "Error::IncorrectStateVariant")
 )]
-#[derive(Debug, Clone, Serialize, Deserialize, Derivative, arbitrary::Arbitrary)]
+#[derive(Debug, Clone, Serialize, Deserialize, Derivative, arbitrary::Arbitrary, TreeHash)]
 #[derivative(PartialEq, Hash(bound = "E: EthSpec"))]
 #[serde(untagged)]
 #[serde(bound = "E: EthSpec, Payload: AbstractExecPayload<E>")]
 #[arbitrary(bound = "E: EthSpec, Payload: AbstractExecPayload<E>")]
+#[tree_hash(enum_behaviour = "transparent")]
 pub struct BeaconBlockBody<E: EthSpec, Payload: AbstractExecPayload<E> = FullPayload<E>> {
     pub randao_reveal: Signature,
     pub eth1_data: Eth1Data,
